857d52e29e01ae6156efb31f484e38ce5eeb8800
Daniel Rodrigues <danielrodrigues@Daniels-MBP-278.hsd1.fl.comcast.net>
2020-04-19 01:44:16 -0400

    Initial structure

---
+++ main.py
@@ -0,0 +1,10 @@
+# ## Folder: Data
+# Includes files regarding data.
+
+# ## Folder: Algorithms 
+# Includes files for each algorithm.
+
+# ## Folder: Testing
+# Includes files for test suites.
+# Includes data partitioning 
+# ... and device suites.
